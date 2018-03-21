a2b69b1cf68bd92f41575978921846f5a7d0ffaa
nzpetter <pgolasz@gmail.com>
2018-03-21 22:25:23 +0100

    default api message

--- application/api/classes/Controller/Welcome.php
+++ application/api/classes/Controller/Welcome.php
@@ -1,10 +1,10 @@
 <?php
 
-class Controller_Welcome extends Controller {
-
-	public function action_index()
-	{
-		$this->response->body('hello, api!');
-	}
+class Controller_Welcome extends Controller
+{
+    public function action_index()
+    {
+        $this->response->status(401)->headers('content-type', 'application/json')->body(json_encode(['status' => -99]));
+    }
 
 } // End Welcome
